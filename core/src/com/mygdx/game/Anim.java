3d591c6ecd9b1c73c7252c535e19c91d702f906f
Sergei Afonin <s.afonin@bankffin.ru>
2022-08-17 01:15:53 +0300

    Homework_2

---
+++ core/src/com/mygdx/game/Anim.java
@@ -0,0 +1,58 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Anim {
+
+    private Texture image;
+    private Animation<TextureRegion> animation;
+    private float time;
+
+    public Anim(String imageName, int columns, int rows, Animation.PlayMode playMode) {
+
+        image = new Texture(imageName);
+        TextureRegion region0 = new TextureRegion(image);
+        int xCnt = region0.getRegionWidth() / columns;
+        int yCnt = region0.getRegionHeight() / rows;
+        TextureRegion[][] regions0 = region0.split(xCnt, yCnt);
+        TextureRegion[] regions = new TextureRegion[regions0.length * regions0[0].length];
+
+        int cnt = 0;
+        for (int i = 0; i < regions0.length; i++) {
+            for (int j = 0; j < regions0[0].length; j++) {
+                regions[cnt++] = regions0[i][j];
+            }
+        }
+
+        this.animation = new Animation<TextureRegion>(1 / 30f, regions);
+        this.animation.setPlayMode(playMode);
+        time += Gdx.graphics.getDeltaTime();
+    }
+
+    public void dispose() {
+        image.dispose();
+    }
+
+    public TextureRegion getFrame() {
+        return animation.getKeyFrame(time);
+    }
+
+    public void setTime(float time) {
+        this.time += time;
+    }
+
+    public void zeroTime() {
+        this.time = 0;
+    }
+
+    public boolean thisAnimationOver() {
+        return animation.isAnimationFinished(time);
+    }
+
+    public void setPlayMode(Animation.PlayMode playMode) {
+        animation.setPlayMode(playMode);
+    }
+}
